623f0e06fad4bdb46bbe96c6cf6b85d463b3462d
trestletech <jeff.allen@trestletechnology.net>
2013-10-26 16:10:18 -0500

    Added example of evaluating R code.

---
+++ inst/examples/02-eval/ui.R
@@ -0,0 +1,23 @@
+library(shiny)
+library(shinyAce)
+
+#' Define UI for application that demonstrates a simple Ace editor
+#' @author Jeff Allen \email{jeff@@trestletech.com}
+shinyUI(
+  bootstrapPage(div(
+    class="container-fluid",
+    div(class="row-fluid",
+        div(class="span6",
+          h2("Source Code"),  
+          aceEditor("code", value="df <- data.frame(num=1:4, 
+  let=LETTERS[2:5], 
+  rand=rnorm(4))
+df"),
+            actionButton("eval", "Evaluate")
+        ),
+        div(class="span6",
+          h2("Output"),
+          verbatimTextOutput("output")
+        )
+  )
+)))
