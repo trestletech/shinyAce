6c7b246e586759ab8a3f8279ab2ecbbef225ddba
vnijs <radiant@rady.ucsd.edu>
2018-04-22 15:29:23 -0700

    renderUI fix

--- inst/examples/07-autocomplete-combine/server.R
+++ inst/examples/07-autocomplete-combine/server.R
@@ -1,5 +1,5 @@
 library(shiny)
-# library(shinyAce)
+library(shinyAce)
 library(dplyr)
 
 shinyServer(function(input, output, session) {
@@ -9,26 +9,29 @@ shinyServer(function(input, output, session) {
     get(input$dataset)
   })
   
-  ## doesn't work as expected
+  comps <- reactive({
+    comps <- list()
+    comps[[input$dataset]] <- colnames(dataset())
+    comps <- c(comps, list(dplyr = getNamespaceExports("dplyr")))
+  })
+  
   output$ace_editor <- renderUI({
     shinyAce::aceEditor(
       "editor",
       mode = "r",
       value = "select(wt, mpg)\n",
       height = "500px",
-      autoComplete = "live"
+      autoComplete = "live",
+      autoCompleteList = comps()
     )
   })
- 
+  
   ## Update static auto complete list according to dataset
   observe({
-    comps <- list()
-    comps[[input$dataset]] <- colnames(dataset())
-    comps <- c(comps, list(dplyr = getNamespaceExports("dplyr")))
     shinyAce::updateAceEditor(session,
       "editor",
       autoCompleters = "static",
-      autoCompleteList = comps
+      autoCompleteList = comps()
     )
   })
 })
--- inst/examples/07-autocomplete-combine/ui.R
+++ inst/examples/07-autocomplete-combine/ui.R
@@ -1,19 +1,7 @@
 library(shiny)
-# library(shinyAce)
-library(dplyr)
 
 shinyUI(fluidPage(
   titlePanel("shinyAce auto completion - combine completion lists"),
   radioButtons("dataset", "Dataset: ", c("mtcars", "airquality"), inline = TRUE),
-  # works as expected
-  # shinyAce::aceEditor(
-  #   "editor",
-  #   mode = "r",
-  #   value = "select(wt, mpg)\n",
-  #   height = "500px",
-  #   autoComplete = "live",
-  #   autoCompleteList = list(dplyr = getNamespaceExports("dplyr"))
-  # )
-  # doesn't work as expected
   uiOutput("ace_editor")
 ))
