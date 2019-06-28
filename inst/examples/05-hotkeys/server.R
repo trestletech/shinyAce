e8d47a8acda87b7185dbdc3a307cac46dcf64da5
vnijs <radiant@rady.ucsd.edu>
2019-06-28 09:28:19 -0700

    examples

--- inst/examples/05-hotkeys/server.R
+++ inst/examples/05-hotkeys/server.R
@@ -11,12 +11,14 @@ shinyServer(function(input, output, session) {
   # the input value (i.e., use `ace_run_key` rather than `run_key`)
   observeEvent(input$ace_run_key, {
     vals$log <- paste(vals$log, renderLogEntry("Run Key"), sep = "\n")
+    print(input$ace_run_key)
   })
 
   # note that the editor `outputId` is prepended to the name of
   # the input value (i.e., use `ace_help_key` rather than `help_key`)
   observeEvent(input$ace_help_key, {
     vals$log <- paste(vals$log, renderLogEntry("Help Key"), sep = "\n")
+    print(input$ace_help_key)
   })
 
   output$log <- renderText({
