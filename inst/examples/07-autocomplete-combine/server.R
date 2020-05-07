f7053dbbda3cdd490014b7f1d6d24bd2a6dd7476
vnijs <radiant@rady.ucsd.edu>
2020-05-07 16:27:27 -0700

    fixed

--- inst/examples/07-autocomplete-combine/server.R
+++ inst/examples/07-autocomplete-combine/server.R
@@ -2,25 +2,19 @@ library(shiny)
 library(shinyAce)
 library(dplyr)
 
-ui <- fluidPage(
-  titlePanel("shinyAce auto completion - combine completion lists"),
-  radioButtons("dataset", "Dataset: ", c("mtcars", "airquality"), inline = TRUE),
-  uiOutput("ace_editor")
-)
-
 server <- function(input, output, session) {
   
   ## Dataset Selection
   dataset <- reactive({
     get(input$dataset)
   })
-  
+
   comps <- reactive({
     comps <- list()
     comps[[input$dataset]] <- colnames(dataset())
     comps <- c(comps, list(dplyr = getNamespaceExports("dplyr")))
   })
-  
+
   output$ace_editor <- renderUI({
     ## initially, only show completions in 'comps' (i.e., dplyr and selected dataset)
     shinyAce::aceEditor(
@@ -33,7 +27,7 @@ server <- function(input, output, session) {
       autoCompleteList = isolate(comps())
     )
   })
-  
+
   ## Update static auto complete list according to dataset and add local completions
   observe({
     shinyAce::updateAceEditor(
@@ -44,7 +38,7 @@ server <- function(input, output, session) {
       autoCompleteList = comps()
     )
   })
-  
+
   ## adding an observer for R-language code completion
   ## will become active after the first switch to another
   ## dataset
@@ -52,5 +46,3 @@ server <- function(input, output, session) {
   ace_annotator <- aceAnnotate("editor")
   ace_tooltip   <- aceTooltip("editor")
 }
-
-shinyApp(ui = ui, server = server)
